504686a96c2d50b79298293603c8f539090b116a
mirash21 <rashidminshin2008@gmail.com>
2026-04-27 00:18:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 # Yandex AI Studio
 YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
-YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID", "b1gfa70gk1jptiabg9eg")
+YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
 YANDEX_API_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1"
 
 # Supabase
